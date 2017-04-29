bffd7b1ae350cedb772fdb36f7c51856081ca4b0
Luis Paniagua <lpaniagua@magnetico.com.ar>
2017-04-29 19:28:26 -0300

    initial base template following example with tiles

---
+++ WebContent/WEB-INF/jsp/contact.jsp
@@ -0,0 +1,10 @@
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>  
+<html>  
+<head>  
+    <title>Spring Tiles Contact Form</title>  
+</head>  
+<body>  
+<h2>Contact Manager</h2>  
+this is my body
+</body>  
+</html>  
---
+++ src/com/springtest/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.springtest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/hello")  
+    public ModelAndView helloWorld() {  
+        String message = "Hello World, Spring MVC @ Javatpoint";  
+        return new ModelAndView("hello", "message", message);  
+    }
+	
+	@RequestMapping("/contact")  
+    public ModelAndView contact() {  
+        return new ModelAndView("contact");  
+    }
+	
+}
